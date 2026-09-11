16e1b4235402ec9d5ac9ebbb8894f140e2ba31c1
BluePig <philipark0903@gmail.com>
2026-09-11 20:26:22 +0900

    edited web.py but not work yet

--- web.py
+++ web.py
@@ -1,27 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
+from io import BytesIO
+
 
 def InnerUrl(id, cpi):
-    return F"https://sldict.korean.go.kr/front/sign/signContentsView.do?current_pos_index={cpi}&origin_no={id}&searchWay=&top_category=&category=CTE001&detailCategory=&searchKeyword=&pageIndex=1&pageJumpIndex="
+    return f"https://sldict.korean.go.kr/front/sign/signContentsView.do?current_pos_index={cpi}&origin_no={id}&searchWay=&top_category=CTE&category=&detailCategory=&searchKeyword=&pageIndex=2&pageJumpIndex="
+
 
 def OuterUrl(page):
-    return F"https://sldict.korean.go.kr/front/sign/signList.do?current_pos_index=9&origin_no=7664&searchWay=&top_category=&category=CTE001&detailCategory=&searchKeyword=&pageIndex={page}&pageJumpIndex="
+    return f"https://sldict.korean.go.kr/front/sign/signList.do?current_pos_index=&origin_no=0&searchWay=&top_category=CTE&category=&detailCategory=&searchKeyword=&pageIndex={page}&pageJumpIndex="
+
 
 def generate_soup(url):
     resp = requests.get(url)
-    soup = BeautifulSoup(resp.content,'html.parser')
+    soup = BeautifulSoup(resp.content, "html.parser")
     return soup
 
-def GetVideo(id, cpi):
-    soup = generate_soup(InnerUrl(id,cpi))
+
+def GetVideoUrl(id, cpi):
+    soup = generate_soup(InnerUrl(id, cpi))
+    vids = soup.select("#html5Video")
+    print(InnerUrl(id, cpi))
+    print(soup)
+    f = open("a.html", "w")
+    f.write(str(soup))
+    for vid in vids:
+        for src_o in vid.select("source"):
+            src = str(src_o.get("src"))
+            if not src.endswith("mp4"):
+                break
+            return src
+    return None
 
 
 soup = generate_soup(OuterUrl(1))
-spans = soup.select('.tit')
+spans = soup.select(".tit")
 for span in spans:
-    href = span.find('a').get('href')
+    a = span.find("a")
+    if a is None:
+        continue
+    href = str(a.get("href"))
     props = href.split("(")[1].split(")")[0].split(",")
     id = props[0].replace("'", "")
-    cpi = props[1].replace("'", "").replace(" ","")
-    GetVideo(id, cpi)
+    cpi = props[1].replace("'", "").replace(" ", "")
+    vurl = GetVideoUrl(id, cpi)
+
+    if vurl is not None:
+        print(vurl)
     break
