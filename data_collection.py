3c2d2385ad470cc167c87dacea16c78d74947b11
taewunia <glipon470@gmail.com>
2026-09-13 20:15:30 +0900

    Add data collection logic and error handling

--- data_collection.py
+++ data_collection.py
@@ -1,75 +1,61 @@
 import cv2
 import mediapipe as mp
+from utills.web import GetVideosFromFile
+import av
+import io
 import numpy as np
-from utills.web import GetVideosFromFile, GetVideoUrl
 
 
-# 국립국어원에서 추출한 MP4 영상 URL
-video_url = GetVideoUrl('01a13035-b352-4375-ae83-d7e5c8c4618b', '0')
-
-# URL을 바로 VideoCapture에 전달
-cap = cv2.VideoCapture('https://sldict.korean.go.kr/multimedia/multimedia_files/convert/20191022/630116/MOV000235141_700X466.mp4')
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    cv2.imshow("Sign Video Stream", frame)
-
-    if cv2.waitKey(30) & 0xFF == ord("q"):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-cap = cv2.VideoCapture(1)
-MAX_EPOCH = 100
-CURRENT_EPOCH = 0
-MAX_FRAME = 30
-data = []
-final_data = []
-zero_data = np.zeros(63)
-action = 'hello'
+def collection(vid):
+    for name, mp4 in vid.items():
 
-while cap.isOpened() and CURRENT_EPOCH < MAX_EPOCH:
-    ret, frame = cap.read()
-    if not ret:
-        print("카메라 오류")
-        break
+        buffer = io.BytesIO(mp4)
+        container = av.open(buffer)
+        video_sequence = []
 
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    result = hands.process(rgb_frame)
+        for frame in container.decode(video=0):
+            cv_image = frame.to_ndarray(format="bgr24")
 
-    if result.multi_hand_landmarks:
-        for hand_landmarks in result.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            left_hand = np.zeros(63)
+            right_hand = np.zeros(63)
+            result = hands.process(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
 
-        joint = []
-        for lm in hand_landmarks.landmark:
-            joint.extend([lm.x, lm.y, lm.z])
-        data.append(np.array(joint))
+            if result.multi_hand_landmarks:
+                for hand_landmarks in result.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(cv_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-    else:
-        data.append(zero_data)
+            cv2.imshow('d', cv_image)
+            if cv2.waitKey(1) & 0XFF == ord('q'):
+                break
 
-    cv2.putText(frame, f"Action:{action} EPOCH:{CURRENT_EPOCH+1}",
-                (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+            if result.multi_hand_landmarks and result.multi_handedness:
+                for hand_landmarks, handness in zip(result.multi_hand_landmarks, result.multi_handedness):
+                    landmarks = []
+                    for lm in hand_landmarks.landmark:
+                        landmarks.extend(([lm.x, lm.y, lm.z]))
 
-    if len(data) == MAX_FRAME:
-        final_data.append(data)
-        CURRENT_EPOCH += 1
-        data = []
+                    label = handness.classification[0].label
 
-    cv2.imshow('test', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                    if label == 'Left':
+                        left_hand = np.array(landmarks)
+                    elif label == 'Right':
+                        right_hand = np.array(landmarks)
 
-cap.release()
-cv2.destroyAllWindows()
-final_data = np.array(final_data)
-print(final_data)
-np.save("data_mountain", final_data)
+            frame_data = np.concatenate([left_hand, right_hand])
+            video_sequence.append(frame_data)
+
+        while len(video_sequence) < 300:
+            video_sequence.append(np.zeros(126))
+        if len(video_sequence) > 300:
+            video_sequence = video_sequence[:300]
+
+        np.save(f"data_{name}", video_sequence)
+    cv2.destroyAllWindows()
+
+vid = GetVideosFromFile()
+collection(vid)
+
